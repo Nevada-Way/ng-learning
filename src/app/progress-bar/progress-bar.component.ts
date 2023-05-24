022a311ad1d5cda040d756273c0aa708c736e467
Nevada-Way <95190960+Nevada-Way@users.noreply.github.com>
2023-05-24 21:54:41 +0300

    Added 2 buttons to add / subtract progress. 4 files modified

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -3,10 +3,28 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html',
-  styleUrls: ['./progress-bar.component.scss']
+  styleUrls: ['./progress-bar.component.scss'],
 })
 export class ProgressBarComponent {
+  @Input() progressPercent: string = '0';
+  progressStep = 10;
 
-  @Input() progressPercent: string = "0";
+  subtractProgress(): string {
+    var newPercent: number = parseInt(this.progressPercent) - this.progressStep;
+    if (newPercent < 0) {
+      newPercent = 0;
+    }
+    this.progressPercent = newPercent + '';
+    console.log('newProgress = ' + this.progressPercent);
+    return this.progressPercent;
+  }
 
+  addProgress(): string {
+    var newPercent: number = parseInt(this.progressPercent) + this.progressStep;
+    if (newPercent > 100) {
+      newPercent = 100;
+    }
+    this.progressPercent = newPercent + '';
+    return this.progressPercent;
+  }
 }
