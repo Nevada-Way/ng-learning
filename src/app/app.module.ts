d9c5960a4a1945de67c70b49a119c2ebf65641e5
Nevada-Way <95190960+Nevada-Way@users.noreply.github.com>
2023-05-24 16:39:02 +0300

    Added progress-bar component, 2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProgressBarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
