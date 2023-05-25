21327ddaaf394b839db824a5fb3fb1913ba2aec7
Nevada-Way <95190960+Nevada-Way@users.noreply.github.com>
2023-05-25 18:06:51 +0300

    Added full functionality, 3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PasswordMakerComponent } from './password-maker/password-maker.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PasswordMakerComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/password-maker/password-maker.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-maker',
+  templateUrl: './password-maker.component.html',
+  styleUrls: ['./password-maker.component.scss'],
+})
+export class PasswordMakerComponent {
+  myPassword = '';
+  myLength = 0;
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+
+  onLengthInput(lengthInput: string) {
+    const parsedValue = parseInt(lengthInput);
+    if (!isNaN(parsedValue)) {
+      this.myLength = parsedValue;
+    }
+    console.log('myLength = ' + this.myLength);
+  }
+
+  onChangeLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onGenerateClick() {
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*';
+    this.myPassword = '';
+
+    let chosenChars = '';
+
+    if (this.useLetters) {
+      chosenChars += letters;
+    }
+    if (this.useNumbers) {
+      chosenChars += numbers;
+    }
+    if (this.useSymbols) {
+      chosenChars += symbols;
+    }
+
+    for (let i = 0; i < this.myLength; i++) {
+      this.myPassword +=
+        chosenChars[Math.floor(Math.random() * chosenChars.length)];
+    }
+  }
+}
